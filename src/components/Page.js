fbec1eecc503617324980ecd1686fe4851479724
Anthony Garvan <anthony.garvan@gmail.com>
2019-10-06 20:47:19 -0500

    progress towards internal representation and fixes a few bugs

--- src/components/Page.js
+++ src/components/Page.js
@@ -7,11 +7,14 @@ import { gfm } from 'turndown-plugin-gfm';
 import _ from 'lodash';
 import marked from 'marked';
 import Shelf from './Shelf';
+import shortid from 'shortid';
 
 
 class Page extends React.Component {
   constructor(props) {
     super(props);
+
+    this.doc = {};
   }
 
   componentDidMount() {
@@ -41,21 +44,30 @@ class Page extends React.Component {
       console.log('selectedBlock:');
       console.log(selectedBlock);
 
-      if(e.key === 'Enter' && e.type === 'keydown' && selectedBlock
-          && selectedBlock[0] && !(sel.anchorNode.data === '\n\u200B' || (sel.anchorNode.tagName === 'BR'))) {
-        console.log(e);
+      if(e.key === 'Enter' && e.type === 'keydown') {
         e.preventDefault();
+
+        // if the current line is not empty, prevent default and continue the string in a newline
+        if(selectedBlock && selectedBlock[0] && !(sel.anchorNode.data === '\n\u200B' || (sel.anchorNode.tagName === 'BR'))) {
+        console.log(e);
 
         let range;
         if(sel.getRangeAt && sel.rangeCount) {
-                range = sel.getRangeAt(0);
-                range.deleteContents();
-                range.insertNode(document.createTextNode('\n\u200B'));
-                sel.anchorNode.nextSibling && sel.collapse(sel.anchorNode.nextSibling, sel.anchorNode.nextSibling.length);
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+            range.insertNode(document.createTextNode('\n\u200B'));
+            sel.anchorNode.nextSibling && sel.collapse(sel.anchorNode.nextSibling, sel.anchorNode.nextSibling.length);
+        }
+        } else {
+          // if the line is empty, start a new paragraph
+          const newBlock = $(`<div id=${shortid.generate()}><br /></div>`);
+          newBlock.insertAfter(selectedBlock);
+          sel.collapse(newBlock[0], 0);
         }
       }
 
-      // make sure selected block is in edit mode
+
+      // enter edit mode, showing markdown
       console.log(selectedBlock.data('editMode'));
       if(selectedBlock && selectedBlock[0] && !selectedBlock.data('editMode')) {
         console.log('markdown:');
@@ -64,7 +76,13 @@ class Page extends React.Component {
         console.log('selection before toggling to edit');
         console.log(sel)
         const anchorOffset = sel.anchorOffset;
-        selectedBlock.html(turndownService.turndown(selectedBlock[0].outerHTML) || '<br />');
+        let renderedMarkdown;
+        if(selectedBlock.attr('id')) {
+          renderedMarkdown = this.doc[selectedBlock.attr('id')];
+        } else {
+          renderedMarkdown = turndownService.turndown(selectedBlock[0].outerHTML) || '<br />'
+        }
+        selectedBlock.html(renderedMarkdown);
         console.log('selection after toggling to edit');
         console.log(sel)
         var range = document.createRange();
@@ -85,7 +103,7 @@ class Page extends React.Component {
         //selectedBlock.css('background-color', '#fafafa');
       }
 
-      // reset the old node upon exit
+      // render the old node upon exit
       if(oldSelectedBlock && oldSelectedBlock[0] && selectedBlock && selectedBlock[0] && !oldSelectedBlock[0].isSameNode(selectedBlock[0])) {
         console.log('rendered markdown:')
         let markdown = oldSelectedBlock[0].innerText.replace(/\u200B/g, '');
@@ -93,13 +111,21 @@ class Page extends React.Component {
         console.log('html:');
         let html = marked(markdown);
         console.log(html);
-        oldSelectedBlock.replaceWith(html.replace(/\\/g, '') || '<p><br /></p>');
+        const renderedNode = $(html.replace(/\\/g, '') || '<div><br /></div>');
+        let id = oldSelectedBlock.attr('id');
+        if(!id) {
+          id = shortid.generate();
+        }
+        renderedNode.attr('id', id);
+        this.doc[id] = markdown;
+        console.log(this.doc);
+        oldSelectedBlock.replaceWith(renderedNode);
       }
 
       // fixes bug with contenteditable where you completely empty the div if the document is empty
       if (e.key === 'Backspace' || e.key === 'Delete') {
           if(!document.querySelector('#m2-page > *')) {
-            document.querySelector('#m2-page').innerHTML = '<div><br /></div>';
+            document.querySelector('#m2-page').innerHTML = `<div id="${shortid.generate()}"><br /></div>`;
           }
       }
     });
